533eb3798f82aab197debe051b8eb4bc64ccbe8d
nico <24nico@gmail.com>
2020-02-05 09:40:11 -0800

    Percents fixed and conditional now exists
    We need to implement logging and get a dedicated server, raspberry pi or school comp?

--- candlez.py
+++ candlez.py
@@ -4,12 +4,13 @@ import pprint
 from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
 from unicorn_fy.unicorn_fy import UnicornFy
 import threading
+import os
 
 symbol_ccxt = 'BNB/USDT'
 symbol_unicorn = "bnbusdt"
 
 change24hr = None
-change1day = None
+change5min = None
 change1hour = None #WORKS
 change1min = None
 change1min_PREV = None
@@ -73,11 +74,19 @@ def stream_minute_candles():
         change1min_PREV = ((close_prev - open_prev) / open_prev) * 100
 
         global change1min
-        close = price
+        close= price
         change1min = ((close - close_prev) / close_prev) * 100
 
-        print("Change1Min:",change1min)
-        print("Change1Min_Prev:",change1min_PREV)
+        time.sleep(1/2)
+
+def stream_5min_candle():
+    while(1):
+        data_hour = exchange.fetchOHLCV(symbol_ccxt, timeframe='5m', limit=2)
+        global change5min
+        open = data_hour[0][4]
+
+        close = price
+        change5min = ((close - open) / open) * 100
         time.sleep(1/2)
 
 
@@ -96,8 +105,10 @@ t1.start()
 min_candles_Thread = threading.Thread(target=stream_minute_candles)
 hr_candles_Thread = threading.Thread(target=stream_hour_candles)
 stream_price_Thread = threading.Thread(target=stream_price)
+min5_candles_Thread = threading.Thread(target=stream_5min_candle)
 time.sleep(3)
 
+min5_candles_Thread.start()
 stream_price_Thread.start()
 min_candles_Thread.start()
 hr_candles_Thread.start()
@@ -105,7 +116,14 @@ time.sleep(2) #let the thread start
 
 
 while(1):
-    # print("1hr->",change1hour)
-    # print("24hr:",change24hr)
+    print("Change1Min:", change1min)
+    print("Change1Min_Prev:", change1min_PREV)
+    print("24Hr:",change24hr)
+    print("5min:",change5min)
 
-    time.sleep(1)
+    conditional = (change1min_PREV>=0.04) and (change1min>=0.07) and (change5min>=0.22) and (change1hour>=0.3) and (change24hr>=4)
+    print(conditional)
+       
+    time.sleep(1/2)
+
+    os.system('cls' if os.name == 'nt' else 'clear') #clears screen
