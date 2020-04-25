f132a35b1ce9cfc6f21c49e130396d71559c31f7
officialnico <24nico@gmail.com>
2020-04-25 17:57:19 -0400

    deleted useless shit

--- apps/raiseTheRoof.py
+++
@@ -1,9 +0,0 @@
-#raseTheRoof utilises binances no fee trading to generate a trading volume of 50 BTC in a span of 30 days
-
-import ccxt
-
-while(1):
-    #submit_order
-    #sell_order
-    #sleep(1)
-    pass
